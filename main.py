b248e4177fd1436b7add730af4e7fca0dd2d7a10
Satwika7 <113924355+Satwika7@users.noreply.github.com>
2022-09-22 05:40:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+random_integer = random.randint(10,100)
+print(random_integer)
+random_float = random.random()
+print(random_float)
+# ways to generate random number greater than 1.0
+print((random_integer)*(random_float))
+print(random_float*5)
